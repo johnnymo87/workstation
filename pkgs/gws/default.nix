5ccfd32a1917413470bc9b0ca18677d21c0ce43e
Jonathan Mohrbacher <jonathan.mohrbacher@gmail.com>
2026-03-07 02:11:10 -0500

    chore(deps): update gws to 0.8.0 (#109)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkgs/gws/default.nix
+++ pkgs/gws/default.nix
@@ -6,20 +6,20 @@
 }:
 
 let
-  version = "0.7.0";
+  version = "0.8.0";
 
   sources = {
     "aarch64-linux" = fetchurl {
       url = "https://github.com/googleworkspace/cli/releases/download/v${version}/gws-aarch64-unknown-linux-musl.tar.gz";
-      hash = "sha256-kDWx0k25g2NSVICzBgVYI1EPSYoIo6ciD3w88NxnX0Q=";
+      hash = "sha256-aJjR148eu+j+bFNqMW3Gm65MfRairwEoCBcD8oNY1y8=";
     };
     "x86_64-linux" = fetchurl {
       url = "https://github.com/googleworkspace/cli/releases/download/v${version}/gws-x86_64-unknown-linux-gnu.tar.gz";
-      hash = "sha256-HOal4PoQuYIJfPK0IRR565VsaWsxmq6uKUWRaeCwv0U=";
+      hash = "sha256-Sy28OxcWqgy/2R1Z6oltv16JOhUVlMNj8MmOdg4uUEY=";
     };
     "aarch64-darwin" = fetchurl {
       url = "https://github.com/googleworkspace/cli/releases/download/v${version}/gws-aarch64-apple-darwin.tar.gz";
-      hash = "sha256-kgeRCL7ZsZf7y3opwSbARZ1dG+s+ntJ32UA5jRQxe2E=";
+      hash = "sha256-k2jJ5YHMJA/RZQSjaX18c3ELei5fxFGmSlaTWO8RZJk=";
     };
   };
 
