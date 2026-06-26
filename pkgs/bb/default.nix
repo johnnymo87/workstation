d83e3d9e86e56fe77942a561794c327228ddb29a
Jonathan Mohrbacher <jonathan.mohrbacher@gmail.com>
2026-06-26 04:37:48 -0400

    chore(deps): update bb to 5.0.389 (#178)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkgs/bb/default.nix
+++ pkgs/bb/default.nix
@@ -16,24 +16,24 @@
 }:
 
 let
-  version = "5.0.388";
+  version = "5.0.389";
 
   sources = {
     "aarch64-linux" = fetchurl {
       url = "https://github.com/buildbuddy-io/bazel/releases/download/${version}/bazel-${version}-linux-arm64";
-      hash = "sha256-nrKf59By+eAqMtq20broQ7sR+9MKqhDsYmcB0DctGPA=";
+      hash = "sha256-yGtOBPIvPJK0w8gI2e/s2/RUETSL9FnDNl9p2Po9A+s=";
     };
     "x86_64-linux" = fetchurl {
       url = "https://github.com/buildbuddy-io/bazel/releases/download/${version}/bazel-${version}-linux-x86_64";
-      hash = "sha256-Qnu+yzw/lEydgMv6j/jIsa+4/dPqBXPVowUMWek8dLg=";
+      hash = "sha256-hnMaQZthVNEmHMrIt2vJf0eHqKZL6dpP18OWB9VIDJI=";
     };
     "aarch64-darwin" = fetchurl {
       url = "https://github.com/buildbuddy-io/bazel/releases/download/${version}/bazel-${version}-darwin-arm64";
-      hash = "sha256-sYEYEzJV8xyAG9CwEhZmy9Tashm8RdrVm6MJOVbqyp8=";
+      hash = "sha256-XpxNczp1UOOmfAAxx8DOH+JP703xNf7JLQqYbK+63k0=";
     };
     "x86_64-darwin" = fetchurl {
       url = "https://github.com/buildbuddy-io/bazel/releases/download/${version}/bazel-${version}-darwin-x86_64";
-      hash = "sha256-5iD9mZuDQgM931nCOfdb9Fso/3NmfSdGM1XS2QTQ/is=";
+      hash = "sha256-uDakwko5wQVlboSe19HVI5UBuk8Kw2G9tpr2FkDmjH0=";
     };
   };
 
